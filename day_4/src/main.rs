cfbbc6936a31efd94c5d631e5df50b9b038d8aad
William Theuer <watheuer@gmail.com>
2023-12-05 10:05:38 -0500

    Day 4 Part 2 solution

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,32 +1,44 @@
 use std::collections::HashSet;
-use std::io::{stdin, BufRead, BufReader};
+use std::io::{BufRead, BufReader, stdin};
 
 struct Card {
     numbers: Vec<u32>,
-    winners: HashSet<u32>
+    winners: HashSet<u32>,
+    index: usize,
 }
 
 fn main() {
     let mut lines = BufReader::new(stdin().lock()).lines();
     let mut cards: Vec<Card> = Vec::new();
+    let mut index = 0usize;
     while let Some(Ok(line)) = lines.next() {
-        cards.push(parse_line(&line));
+        cards.push(parse_line(index, &line));
+        index += 1;
     }
+
+    // num of duplicates of each card
+    let mut duplicates = [1u32; 219];
 
     let mut total = 0;
-    for Card { winners, numbers } in cards {
-        let mut points = 0;
+    for Card {
+        numbers,
+        winners,
+        index,
+    } in cards
+    {
+        let mut next_index = 1;
         for number in numbers {
             if winners.contains(&number) {
-                points = if points == 0 { 1 } else { points * 2 };
+                duplicates[index + next_index] += duplicates[index];
+                next_index += 1;
             }
         }
-        total += points;
+        total += duplicates[index];
     }
-    println!("Total points: {total}");
+    println!("Total cards: {total}");
 }
 
-fn parse_line(line: &str) -> Card {
+fn parse_line(i: usize, line: &str) -> Card {
     let winner_strings: &Vec<&str> = &line[10..39].split_ascii_whitespace().collect();
     let number_strings: &Vec<&str> = &line[42..116].split_ascii_whitespace().collect();
 
@@ -39,5 +51,6 @@ fn parse_line(line: &str) -> Card {
             .iter()
             .map(|s| s.parse().expect("Invalid number on card"))
             .collect(),
+        index: i,
     }
 }
