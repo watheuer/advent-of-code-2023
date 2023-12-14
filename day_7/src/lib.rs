4c25d4e176bace279dcd33668f50f0c8f6476869
William Theuer <watheuer@gmail.com>
2023-12-14 00:42:54 -0500

    Ugly day 7 part 2

--- day_7/src/lib.rs
+++ day_7/src/lib.rs
@@ -1,5 +1,5 @@
 mod hands {
-    use std::cmp::Ordering;
+    use std::cmp::{Ordering};
     use std::collections::HashMap;
 
     #[derive(Debug, Ord, Eq, PartialEq, PartialOrd)]
@@ -35,6 +35,75 @@ mod hands {
             }
         }
 
+        pub fn from_str_with_jokers(hand_val: &str, bid: usize) -> Self {
+            let card_strengths: [u8; 5] = hand_val
+                .chars()
+                .map(Hand::char_to_strength_joker)
+                .collect::<Vec<u8>>()
+                .try_into()
+                .expect("Invalid hand");
+            let num_jokers = card_strengths.iter().filter(|num| **num == 0).count();
+            let hand_type = Hand::get_hand_type_modified(card_strengths, num_jokers);
+            Hand {
+                cards: card_strengths,
+                hand_type,
+                bid
+            }
+        }
+
+        fn get_hand_type_modified(hand: [u8; 5], num_jokers: usize) -> HandType{
+            let mut map: HashMap<u8, usize> = HashMap::new();
+            let mut max = 0;
+            let mut max_card: Option<u8> = None;
+            for card in hand {
+                // Only count non-jokers
+                if card != 0 {
+                    let new_count = match map.get(&card) {
+                        Some(count) => count + 1,
+                        None => 1
+                    };
+                    if new_count > max {
+                        max = new_count;
+                        max_card = Some(card);
+                    }
+                    map.insert(card, new_count);
+                }
+            }
+
+            // Increment top card count by joker count
+            if let Some(card) = max_card {
+                map.insert(card, *map.get(&card).unwrap() + num_jokers);
+            }
+
+            match map.len() {
+                // 5 of a kind or all jokers
+                0 | 1 => HandType::FiveOfAKind,
+                // 4 of a kind or full house
+                2 => {
+                    for val in map.values() {
+                        if *val == 4 {
+                            return HandType::FourOfAKind;
+                        }
+                    }
+                    HandType::FullHouse
+                }
+                // 3 of a kind or two pair
+                3 => {
+                    for val in map.values() {
+                        if *val == 3 {
+                            return HandType::ThreeOfAKind;
+                        }
+                    }
+                    HandType::TwoPair
+                }
+                // One pair
+                4 => HandType::OnePair,
+                // High card
+                5 => HandType::HighCard,
+                _ => panic!("Invalid hand type"),
+            }
+        }
+
         fn get_hand_type(hand: [u8; 5]) -> HandType {
             let mut map: HashMap<u8, u8> = HashMap::new();
             for card in hand {
@@ -50,7 +119,7 @@ mod hands {
                 // 4 of a kind or full house
                 2 => {
                     for val in map.values() {
-                        if *val == 4u8 {
+                        if *val == 4 {
                             return HandType::FourOfAKind;
                         }
                     }
@@ -59,7 +128,7 @@ mod hands {
                 // 3 of a kind or two pair
                 3 => {
                     for val in map.values() {
-                        if *val == 3u8 {
+                        if *val == 3 {
                             return HandType::ThreeOfAKind;
                         }
                     }
@@ -85,6 +154,25 @@ mod hands {
                 '9' => 7,
                 'T' => 8,
                 'J' => 9,
+                'Q' => 10,
+                'K' => 11,
+                'A' => 12,
+                _ => panic!("Invalid card"),
+            }
+        }
+
+        fn char_to_strength_joker(c: char) -> u8 {
+            match c {
+                'J' => 0,
+                '2' => 1,
+                '3' => 2,
+                '4' => 3,
+                '5' => 4,
+                '6' => 5,
+                '7' => 6,
+                '8' => 7,
+                '9' => 8,
+                'T' => 9,
                 'Q' => 10,
                 'K' => 11,
                 'A' => 12,
@@ -144,6 +232,30 @@ mod tests {
                 .parse()
                 .expect("Invalid bid");
             let hand = Hand::from_str(&hand_val, bid);
+            hands.push(hand);
+        }
+        hands.sort();
+        let mut total_winnings = 0;
+        for (i, hand) in hands.iter().enumerate() {
+            let rank = i + 1;
+            total_winnings += rank * hand.bid;
+        }
+        println!("Total winnings: {total_winnings}");
+    }
+
+    #[test]
+    fn part_2() {
+        let mut lines = BufReader::new(File::open("input").expect("Missing input")).lines();
+        let mut hands: Vec<Hand> = Vec::new();
+        while let Some(Ok(line)) = lines.next() {
+            let mut segments = line.split_whitespace();
+            let hand_val = segments.next().expect("Missing hand value");
+            let bid: usize = segments
+                .next()
+                .expect("Missing bid")
+                .parse()
+                .expect("Invalid bid");
+            let hand = Hand::from_str_with_jokers(&hand_val, bid);
             hands.push(hand);
         }
         hands.sort();
